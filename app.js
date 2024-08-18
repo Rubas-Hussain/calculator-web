2dc03d8f2415757ba5eaf147a16ed35616eb72a2
Rubas <rubasakhter.11@gmail.com>
2024-08-18 14:21:20 +0500

    theme adjusted

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let string = '';
 let btns = document.querySelectorAll('.btns');
 let input = document.querySelector('input');
 let result = document.querySelector('#result');
+let moonAndSunContainer=document.querySelector('#themeIcon')
 let moon = document.querySelector('.fa-moon');
 let appWrapper = document.querySelector('#appWrapper');
 
@@ -12,20 +13,20 @@ Array.from(btns).forEach((button) => {
         if (btnValue == '=') {
             try {
                 string = string.replace(/x/g, '*').replace(/÷/g, '/').replace(/(\d+(\.\d+)?)%/g, '($1 * 0.01');
-    
+
                 string = eval(string);
-                result.innerHTML = string; 
+                result.innerHTML = string;
             } catch (error) {
-                input.value = "Error"; 
+                input.value = "Error";
             }
         } else if (btnValue == 'AC') {
-            string = ''; 
+            string = '';
             input.value = '';
         } else {
-            if(result.innerHTML || input.value=='Error'){
-                string = ''; 
+            if (result.innerHTML || input.value == 'Error') {
+                string = '';
                 input.value = '';
-                result.innerHTML='';
+                result.innerHTML = '';
             }
             string += btnValue;
             input.value = string;
@@ -33,13 +34,23 @@ Array.from(btns).forEach((button) => {
     });
 });
 
-let isWhite=true;
-moon.addEventListener('click',(e)=>{
-    if(isWhite){
-        appWrapper.style.backgroundColor='#283637';
-        isWhite=!isWhite;
-    }else{
-        appWrapper.style.backgroundColor='#ffffff';
-        isWhite=!isWhite;
+let isWhite = true;
+moonAndSunContainer.addEventListener('click', (e) => {
+    if (isWhite) {
+        appWrapper.style.backgroundColor = '#283637';
+        btns.forEach((button) => {
+            button.style.color = '#f5f5f5'
+        });
+        input.style.backgroundColor='#283637';
+        moonAndSunContainer.innerHTML=`<i class="fa-regular fa-sun icons"></i>`;
+        isWhite = !isWhite;
+    } else {
+        appWrapper.style.backgroundColor = '#ffffff';
+        btns.forEach((button) => {
+            button.style.color = '#283637'
+        });
+        input.style.backgroundColor='#ffffff'
+        moonAndSunContainer.innerHTML=`<i class="fa-solid fa-moon fa-xl icons"></i>`;
+        isWhite = !isWhite;
     }
 });
